2f13559f4d6f44725467e63fdf7bc3680f29341f
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2020-04-29 17:39:35 +0530

    new changes

--- reorganise/appone/newapp/views.py
+++ reorganise/appone/newapp/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 28 22:53:45 2020
 
-# Create your views here.
+@author: abhijithneilabraham
+"""
+
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from appone.newapp.serializers import UserSerializer, GroupSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]
